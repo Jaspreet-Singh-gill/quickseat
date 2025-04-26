a6fe11e7738eaf39da9b7f53d6a997df52d79a56
jaspreet singh <jaspreetsinghdall@gmail.com>
2025-04-26 19:31:03 +0530

    added file4

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://jaspr:nwRO4p69I1l4uYplRgc4zJLq5DFvGvKs@dpg-d06dqnjuibrs73eg3hs0-a.oregon-postgres.render.com:5432/database_2q1g"
+SQLALCHEMY_DATABASE_URL = "postgresql://jaspr:nwRO4p69I1l4uYplRgc4zJLq5DFvGvKs@dpg-d06dqnjuibrs73eg3hs0-a/database_2q1g"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
